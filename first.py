474c253da065a4e504a3dc485b59e90f8880c868
alamsaha945-sudo <alamsaha945@gmail.com>
2026-02-10 19:49:16 +0530

    Day 1: Simple discount checker python python code

---
+++ first.py
@@ -0,0 +1,6 @@
+# simplle discount chekar
+user=input("jamar maf khoto inch?")
+if int(user)>18:
+       print("apni 50 taka discount pabe ")
+else:
+       print("apni koni discount panen na ")       
