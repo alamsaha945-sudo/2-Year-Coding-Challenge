0322ebad586ecdd4c357db8a3a75e7e2afb3ffd3
alamsaha945-sudo <alamsaha945@gmail.com>
2026-02-12 02:40:19 +0530

    day 3: grocery store billing system with discount logic and loops

---
+++ day3.py
@@ -0,0 +1,14 @@
+# grocery store billing systeam whith discount logic and loops
+mot_bazar=int(input("khoto takar bazar korle?")) 
+print("tomar mot bazar",mot_bazar)
+if mot_bazar>=2000:
+    print("apni 100 tka chaar paben")
+    bill=mot_bazar-100
+    print("char diye apni pelen",bill)
+    for i in range(5):   
+        print("congarast")
+else:
+  print("apni kono char paben na ")
+
+    
+
