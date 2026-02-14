499d5d5a5cbc48afe86accc3f2d28e7c71c414eb
alamsaha945-sudo <alamsaha945@gmail.com>
2026-02-14 10:48:02 +0530

    day 5: user login system using python.

---
+++ day5.py
@@ -0,0 +1,12 @@
+# user login sestem using python functions and return statements
+def user_login(username, password):
+    if username==12367 and password==989898:
+        return"confrom"
+    else:
+        return"try again letter"
+chake1=user_login(12367,989898)
+print(chake1)
+
+
+    
+    
