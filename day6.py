eacdf787137c7a047703b5237f821977b317fdab
alamsaha945-sudo <alamsaha945@gmail.com>
2026-02-15 18:13:50 +0530

    day 6:smart trailer invoice system.

---
+++ day6.py
@@ -0,0 +1,13 @@
+# smart trailer invoice system
+user_name=input("what is you name")
+def bill_calclator(drees, contity,per_rate):
+    mot_dam=contity*per_rate
+    return mot_dam
+oders=["panjabi""jama""pant"]
+for posak in oders:
+   koita= int(input("koyta "+posak +"lagbe"))
+   rate=bill_calclator(posak,koita,200)
+   if rate>1000:
+    print("apni specal char paben\napni amader kache khub valo costumer")
+   else:
+    print("amader pase takun thanks")    
