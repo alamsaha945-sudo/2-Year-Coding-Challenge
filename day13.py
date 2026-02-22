6a412e9db35d513644c14295db11b98c6b159bcc
alamsaha945-sudo <alamsaha945@gmail.com>
2026-02-22 18:56:42 +0530

    day 13:digital customer list manager

---
+++ day13.py
@@ -0,0 +1,13 @@
+# digital customer list manager
+customers=["saha","sabij","masum","asanur","rahul"]
+customers.append("alam")
+print(customers)
+customers.pop(1)
+print(customers)
+print(len(customers))
+customers.sort()
+print(customers)
+customers.reverse()
+print(customers)
+customers.insert(1,"jahir")
+print(customers)
