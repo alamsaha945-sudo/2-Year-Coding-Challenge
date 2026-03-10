a33525093398f30fef45a171e200fa9f63808cc4
alamsaha945-sudo <alamsaha945@gmail.com>
2026-03-10 13:59:11 +0530

    Day 29:Logic and list reference

---
+++ day_29.py
@@ -0,0 +1,16 @@
+# Logic and list reference 
+s="saha"
+if s:
+    print("yes")
+else:
+    print("no")
+a="3"
+b=2
+print(a*b)
+data=[1,2,3,] 
+result=data
+result.append(5)
+if data==result:
+    print("yes")
+else:
+    print("no")          
